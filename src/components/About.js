4edecc050e4dba1c170689c67a2c1fed67a2e2da
dhivya-GD <dhivyasekarp20@gmail.com>
2023-05-31 12:18:03 +0530

    changes in About us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,21 @@
 const About = () => {
     return(
-       <div className="About-Us">Hello this is about us Page</div>
+       <div className="About-Us">
+        <div>
+        <h3>We Ask the Right Questions About Your Business</h3>
+        <div>Before we start any project, we take the time to get to know your business. We ask the right questions to gain a deep understanding of your business, target audience, and competition.</div>
+        </div>
+
+        <div>
+        <h3>Developing Customized Solutions</h3>
+        <div>Once we have a clear understanding of your business, we work alongside you to develop customized solutions that meet your specific needs. We deliver a solution that meets your expectations.</div>
+        </div>
+
+        <div>
+        <h3>Launching Your Solutions</h3>
+        <div>We don’t just develop your solutions and leave you to it. We work alongside you to launch your solutions and provide ongoing support and maintenance to ensure that they continue to meet your needs over time.</div>
+        </div>
+       </div>
     );
 }
 export default About;
